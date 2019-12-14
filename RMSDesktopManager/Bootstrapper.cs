e179ffeef0a1434849349b5185f78b4c666685fe
syedhabeebuddin <syedhabeebuddin0@gmail.com>
2019-12-14 18:49:51 +0530

    Add Desktop Manager
    Adds the WPF Desktop solution.

---
+++ RMSDesktopManager/Bootstrapper.cs
@@ -0,0 +1,58 @@
+﻿using Caliburn.Micro;
+using RMSDesktopManager.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RMSDesktopManager
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private SimpleContainer simpleContainer = new SimpleContainer();
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            simpleContainer.Instance(simpleContainer);
+            simpleContainer
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => simpleContainer.RegisterPerRequest(
+                    viewModelType,viewModelType.ToString(),viewModelType));
+
+            //base.Configure();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            //base.OnStartup(sender, e);
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return simpleContainer.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return simpleContainer.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            simpleContainer.BuildUp(instance);
+        }
+    }
+}
